31bcb6f71b56867913e67375fefeed781148fcf4
Vitali Dolgoshei <Vitald75@gmail.com>
2021-12-02 20:48:09 +0300

    HW4 All tasks

---
+++ specs/Buy_currency.js
@@ -0,0 +1,53 @@
+describe("Buy currency", function () {
+ 
+  before("Login with credentials walker@jw.com:password", async function () {
+    await browser.url(
+      "https://viktor-silakov.github.io/course-sut/index.html?quick"
+    );
+    await $("#login").setValue("walker@jw.com");
+    await $("#password").setValue("password");
+    await $("button").click();
+    await $("#spinner").waitForDisplayed({
+      reverse: true,
+      timeout: 5000,
+    });
+  });
+
+  it("Should enter 1000 in field currency, click buy, check result amount", async function () {
+    async function inputOneNumber (value, position) {
+      await inputSum.addValue(value);
+      await browser.waitUntil(
+        async () => {
+          const data = await $("#database").getHTML("false");
+          const newdata = data.slice(data.indexOf("["), data.indexOf("]") + 1);
+          const dataJson = JSON.parse(newdata);
+          return (dataJson[position].num == value)
+        },
+        {
+            timeout: 5000,
+            interval: 300,
+            timeoutMsg: "The element is not in databaswe",
+        }
+    )}
+  
+    await expect(browser).toHaveTitle("Report portal");
+    const currencyRate = await $("#currency-rate").getText();
+    const inputSum = await $("#sum-to-buy");
+    await inputOneNumber("1", 0);
+    await inputOneNumber("0", 1);
+    await inputOneNumber("0", 2);
+    await inputOneNumber("0", 3);
+    
+    await $("//button[text()='Buy']").click();
+    const sum = await inputSum.getValue();
+    expectedAmount = Number(sum) * Number(currencyRate);
+    
+    const resultElement = await $("#withdrew");
+    await expect(resultElement).toBeDisplayed();
+    const result = await resultElement.getText();
+    const actualAmoutStr = result.slice(result.indexOf(">")+1).trim();
+    const actualAmout = Number(actualAmoutStr);
+    
+    await expect(expectedAmount).toEqual(actualAmout);
+  });
+});
