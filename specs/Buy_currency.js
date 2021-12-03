d02cc52d9c64bdb2fd985ad9aebc140dcb3fa8e1
Dolgoshei Vitali <Vitald75@gmail.com>
2021-12-03 17:16:00 +0300

    HW4

--- specs/Buy_currency.js
+++ specs/Buy_currency.js
@@ -1,5 +1,4 @@
 describe("Buy currency", function () {
- 
   before("Login with credentials walker@jw.com:password", async function () {
     await browser.url(
       "https://viktor-silakov.github.io/course-sut/index.html?quick"
@@ -14,40 +13,43 @@ describe("Buy currency", function () {
   });
 
   it("Should enter 1000 in field currency, click buy, check result amount", async function () {
-    async function inputOneNumber (value, position) {
+    async function inputOneNumber(value, position) {
       await inputSum.addValue(value);
       await browser.waitUntil(
         async () => {
-          const data = await $("#database").getHTML("false");
-          const newdata = data.slice(data.indexOf("["), data.indexOf("]") + 1);
+          const data = await $("#database").getHTML(false);
+          console.log (data);
+          //const newdata = data.slice(data.indexOf("["), data.indexOf("]") + 1);
+          const newdata = data;        
           const dataJson = JSON.parse(newdata);
-          return (dataJson[position].num == value)
+          return dataJson[position].num == value;
         },
         {
-            timeout: 5000,
-            interval: 300,
-            timeoutMsg: "The element is not in databaswe",
+          timeout: 5000,
+          interval: 300,
+          timeoutMsg: "The element is not in databaswe",
         }
-    )}
-  
+      );
+    }
+
     await expect(browser).toHaveTitle("Report portal");
     const currencyRate = await $("#currency-rate").getText();
     const inputSum = await $("#sum-to-buy");
     await inputOneNumber("1", 0);
-    await inputOneNumber("0", 1);
-    await inputOneNumber("0", 2);
-    await inputOneNumber("0", 3);
-    
+    await inputOneNumber("2", 1);
+    await inputOneNumber("3", 2);
+    await inputOneNumber("4", 3);
+
     await $("//button[text()='Buy']").click();
     const sum = await inputSum.getValue();
     expectedAmount = Number(sum) * Number(currencyRate);
-    
+
     const resultElement = await $("#withdrew");
     await expect(resultElement).toBeDisplayed();
     const result = await resultElement.getText();
-    const actualAmoutStr = result.slice(result.indexOf(">")+1).trim();
+    const actualAmoutStr = result.slice(result.indexOf(">") + 1).trim();
     const actualAmout = Number(actualAmoutStr);
-    
+
     await expect(expectedAmount).toEqual(actualAmout);
   });
 });
