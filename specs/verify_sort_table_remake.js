97d2030852a8727cafb6fe8192208cfd109f1a50
Dolgoshei Vitali <Vitald75@gmail.com>
2021-12-08 18:03:52 +0300

    HW4 verify_sorting_tabke_remake

--- specs/verify_sort_table_remake.js
+++ specs/verify_sort_table_remake.js
@@ -20,26 +20,8 @@ describe("Verify sorting at the table Week leaders", function () {
     }) 
   });
 
-  async function verifySorting(field) {
-    function assert(sorting, arrayfromTable, arraySorted, arraySortedReverse) {
-      if (sorting === "asc") {
-        expect(arrayfromTable).toEqual(arraySorted);
-      } else if (sorting === "desc") {
-        expect(arrayfromTable).toEqual(arraySortedReverse);
-      } else {
-        throw new Error("Error in determening sorting order");
-      }
-    }
- 
-    const sortingType = await $(
-      `div[tabulator-field='${field}'][role='columnheader']`
-    ).getAttribute("aria-sort");
-    const arrayfromTable = await $$(
-      `div[role='gridcell'][tabulator-field='${field}']`
-    ).map((x) => {
-      return x.getText();
-    });
-    const arraySorted = arrayfromTable
+  function verifySorting(arrayFromTable, sorting) {
+    const arraySorted = arrayFromTable
       .map((x) => x)
       .sort(function (a, b) {
         if (a > b) {
@@ -51,85 +33,94 @@ describe("Verify sorting at the table Week leaders", function () {
         return 0;
       });
     const arraySortedReverse = arraySorted.map((x) => x).reverse();
-    assert(sortingType, arrayfromTable, arraySorted, arraySortedReverse);
-  }
-
-async function verifyArrow(field) {
-    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
-    const arrowElement = await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-arrow`);
-    const arrowTopStyleCSS = await arrowElement.getCSSProperty('borderTopStyle');
-    const arrowTopWidthCSS = await arrowElement.getCSSProperty('borderTopWidth');
-    const arrowBottomStyleCSS = await arrowElement.getCSSProperty('borderBottomStyle');
-    const arrowBottomWidthCSS = await arrowElement.getCSSProperty('borderBottomWidth');
-    const arrowTopStyle = arrowTopStyleCSS.value; 
-    const arrowTopWidth = arrowTopWidthCSS.value; 
-    const arrowBottomStyle = arrowBottomStyleCSS.value; 
-    const arrowBottomWidth = arrowBottomWidthCSS.value; 
-    
-    if (sortingType === "asc") {
-      expect(arrowBottomStyle + arrowBottomWidth).toEqual("solid6px");
-    }
-    else if (sortingType === "desc") {
-      expect(arrowTopStyle + arrowTopWidth).toEqual("solid6px");
+    if (sorting === "asc") {
+      expect(arrayFromTable).toEqual(arraySorted);
+    } else if (sorting === "desc") {
+      expect(arrayFromTable).toEqual(arraySortedReverse);
     } else {
       throw new Error("Error in determening sorting order");
     }
-}
+  }
 
-context("Should verify if sorting by field Id is correct", async function (){
+  context("Should verify if sorting by field Id is correct", async function (){
 
-  it("should click on Id header first time", async function () {
+  it("should click first time on Id header - ascending", async function () {
     const field = "id";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);    
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    expect(sortingType).toEqual("asc");
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'asc');
   });
 
-  it("should click second time on IDd header", async function () {
+  it("should click second time on Id header - descending", async function () {
     const field = "id";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    await expect(sortingType).toEqual("desc");
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'desc');
   });
 })
 
 context("Should verify if sorting by field Name is correct", async function (){
  
-  it("should click on Name header first time", async function () {
+  it("should click first time on Name header - ascending", async function () {
     const field = "name";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);    
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    expect(sortingType).toEqual("asc");
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'asc'); 
   });
 
-  it("should click second time on Name header", async function () {
+  it("should click second time on Name header - descending", async function () {
     const field = "name";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    expect(sortingType).toEqual("desc");
+
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'desc');
   });
 })
 
 context("Should verify if sorting by field Age is correct", async function (){
 
-  it("should click on Age header first time", async function () {
+  it("should click first time on Age header - ascending", async function () {
     const field = "age";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);    
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    expect(sortingType).toEqual("asc");
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'asc'); 
   });
 
-  it("should click second time on Age header", async function () {
+  it("should click second time on Age header descending", async function () {
     const field = "age";
     await $(`div[tabulator-field='${field}'][role='columnheader'] div.tabulator-col-content`).click();
     await browser.pause(500);
-    await verifyArrow(field);
-    await verifySorting (field);
+    const sortingType = await $(`div[tabulator-field='${field}'][role='columnheader']`).getAttribute("aria-sort");
+    expect(sortingType).toEqual("desc");
+    const arrayfromTable = await $$(`div[role='gridcell'][tabulator-field='${field}']`).map((x) => {
+      return x.getText();
+    });  
+    verifySorting(arrayfromTable, 'desc');
   });
 })
 })
